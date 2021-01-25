686c7650c471a98a2568b1212c18ccb5124bfe23
makhlouf.yassine <makhlouf.yassine@hotmail.fr>
2021-01-25 17:20:46 +0000

    get filenames

--- CPA_on_RSA.py
+++ CPA_on_RSA.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 curves = []
 
 for curve_name in os.listdir('Ressources'):
-    curves.append(curve_name)
+    file_rd = open(curve_name)
+    file_rd.readlines()
+    
 
 print("curves:\n", curves)

60988cff42028cb857d057b0a72fc956bc121237
makhlouf.yassine <makhlouf.yassine@hotmail.fr>
2021-01-25 18:36:27 +0000

    solution

--- CPA_on_RSA.py
+++ CPA_on_RSA.py
@@ -1,7 +1,19 @@
 import os
+import sympy
 import matplotlib.pyplot as plt
 
 print("Welcome to Axel's and Yassine's baby RSA !")
+
+def solution():
+    N = 95559869
+    E = 65535
+    P = 2
+    while (N%P != 0):
+        P = sympy.nextprime(P)
+    Q = int(N/P)
+    print("P: ", P)
+    print("Q: ", Q) 
+
 
 def curve():
     curves = []
@@ -22,17 +34,22 @@ def generate(name, number):
                     result.append(int(line.split()[0]))
     return result
 
-# find N in the files
+# find N in the files 95559869
 N = open("Ressources/N.txt", "r").readlines()[0]
 print(f"N : {N}")
+solution()
 
 # get the curves from the files
-curves = generate("curve_", 999)
+curves = generate("curve_", 1000)
 x_curves = [k for k in range(len(curves[0]))]
 
 # get the messages from the files
-messages = generate("msg_", 999)
+messages = generate("msg_", 1000)
 
+# public exponent
+E = 65535
+
+# show the results
 plt.plot(x_curves, curves[0])
 plt.axis([0, len(x_curves), 0, max(curves[0])])
 plt.xlabel("time")
